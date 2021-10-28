c1ee1db1613bac283eb2fc76f26a2999c8e12170
ompl <js211@rice.edu>
2021-10-28 16:16:19 -0500

    graph added

--- project4/visualization.py
+++ project4/visualization.py
@@ -2,7 +2,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy
 import matplotlib.pyplot as plt
 
-data = numpy.loadtxt('path.txt')
+data = numpy.loadtxt('Pendulum RRT T10.txt')
 #fig = plt.figure()
 #ax = fig.gca(projection = '3d')
 #ax.plot(data[:,0],data[:,1])
